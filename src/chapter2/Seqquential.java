5ad0b3952129b97521c11eaeb3a44afe47d25c2d
liuteng <liuteng0524@126.com>
2013-11-07 23:19:43 +0800

    添加main测试方法，调整原有代码结构

--- src/chapter2/Seqquential.java
+++ src/chapter2/Seqquential.java
@@ -1,18 +1,19 @@
 package chapter2;
 
-public class Seqquential {
+import java.util.Scanner;
+
+import sun.security.krb5.internal.PAData;
+
+class DATA {
+	String key;
+	String name;
+	int age;
+}
+
+class SLType {
 	static final int MAXLEN = 100;
-
-	class DATA {
-		String key;
-		String name;
-		int age;
-	}
-
-	class SLType {
-		DATA[] ListData = new DATA[MAXLEN + 1];
-		int Listlen;
-	}
+	DATA[] ListData = new DATA[MAXLEN + 1];
+	int Listlen;
 
 	/**
 	 * 初始化顺序序
@@ -103,7 +104,7 @@ public class Seqquential {
 	 * @return
 	 */
 	DATA SLFindByNum(SLType SL, int n) {
-		if (n < 0 || n > SL.Listlen) {
+		if (n < 1 || n > SL.Listlen+1) {
 			System.out.println("结点语错误，不能返回结点！");
 			return null;
 		}
@@ -129,15 +130,57 @@ public class Seqquential {
 
 	/**
 	 * 显示所有结点
+	 * 
 	 * @param SL
 	 * @return
 	 */
 	int SLAll(SLType SL) {
 		int i;
 		for (i = 1; i <= SL.Listlen; i++) {
-			System.out.printf("(%s,%s,%d\n)", SL.ListData[i].key,
+			System.out.printf("(%s,%s,%d)\n", SL.ListData[i].key,
 					SL.ListData[i].name, SL.ListData[i].age);
 		}
 		return 0;
+	}
+}
+
+public class Seqquential {
+	public static void main(String[] args) {
+		int i;
+		SLType SL = new SLType();
+		DATA pdata;
+		String key;
+		System.out.println("顺序表操作演示！");
+		SL.SLInit(SL);
+		Scanner input = new Scanner(System.in);
+		do {
+			System.out.println("输入添加的结点（学号 姓名 年龄）：");
+			DATA data = new DATA();
+			data.key = input.next();
+			data.name = input.next();
+			data.age = input.nextInt();
+			if (data.age != 0) {
+				if (SL.SLAdd(SL, data) == 0) {
+					break;
+				}
+			} else {
+				break;
+			}
+		} while (true);
+		System.out.println("\n顺序表中的顺序为：");
+		SL.SLAll(SL);
+		System.out.println("\n要取出的结点的序号为：");
+		i = input.nextInt();
+		pdata = SL.SLFindByNum(SL, i);
+		if (pdata != null) {
+			System.out.printf("第%d个结点为：(%s,%s,%d)", i, pdata.key, pdata.name,pdata.age);
+		}
+		System.out.printf("\n要查找结点的关键字：");
+		key = input.next();
+		i = SL.SLFindByCout(SL, key);
+		pdata = SL.SLFindByNum(SL, i);
+		if (pdata != null) {
+			System.out.printf("第%d个结点为：(%s,%s,%d)", i, pdata.key, pdata.name,pdata.age);
+		}
 	}
 }
